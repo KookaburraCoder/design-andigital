6e3ef90531d7017fceef914c9b13b065b9809bb5
Jamie Carroll <jamiecarroll.au@icloud.com>
2022-02-03 13:37:50 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="content" style={{marginTop: 127}}>
+        <div id="logo">D</div>
+        <h1>anDigital</h1>
+        <h2>Digital Media Solutions</h2>
+        <h3>My Time is for Clients… Not to Design this Website.</h3>
+        <ul>
+          <li>Website Development</li>
+          <li>Graphic Design</li>
+          <li>Search Engine Optimisation (SEO)</li>
+          <li>Branding</li>
+          <li>Automation</li>
+          <li>Image Retouching</li>
+        </ul>
+        <p>Email me at <a href="mailto:random8140@hotmail.com">random8140@hotmail.com</a> for a free quote.</p>
+      </div>
     </div>
   );
 }
